0be1247d7ed31a89f26bf979121b4602920d247c
jihyeon <5dbswo@naver.com>
2019-12-11 10:23:03 +0900

    setup

---
+++ src/main/java/com/java/aop/HomeAscpect.java
@@ -0,0 +1,33 @@
+package com.java.aop;
+
+import java.util.logging.Logger;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class HomeAscpect {
+	public static Logger logger = Logger.getLogger(HomeAscpect.class.getName());
+	public static final String logMsg = "LogMsg==================";
+	
+	@Around(value = "within(com.java..*)")
+	public Object advice(ProceedingJoinPoint joinPoint)throws Throwable{
+		Object obj=null;
+		
+		try {
+			
+			logger.info(logMsg + joinPoint.getTarget().getClass().getName() + "\t\t" + joinPoint.getSignature().getName());
+			obj=joinPoint.proceed();
+			
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		return obj;
+		
+	}
+	
+	/* annotation 설정 후 servlet.xml에서 component-scan, autoproxy설정 */
+}
---
+++ src/main/webapp/WEB-INF/views/tempalte/layout.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="t" uri="http://tiles.apache.org/tags-tiles" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<t:insertAttribute name="header"/>
+	<hr color="blue"/>
+	
+	<t:insertAttribute name="menu"/>
+	<hr color="pink"/>
+	
+	<t:insertAttribute name="content"/>
+	<hr color="green"/>
+	
+	<t:insertAttribute name="foot"/>
+</body>
+</html>
